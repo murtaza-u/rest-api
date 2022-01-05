bf225f0427810ef3fe78a1f11c31fe3c25029cfb
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-01-05 09:40:56 +0530

    implemented handlers for requests to our rest-api

---
+++ handlers.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+func get(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func update(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func delete(w http.ResponseWriter, r *http.Request) {
+
+}
